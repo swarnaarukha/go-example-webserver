49d4b4772791796f709e4c81bd87933b9178f173
Arukha <swarnalata.arukha@capgemini.com>
2017-03-29 14:39:31 +0100

    .drone.yml

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -eu
 ID=$! # ID of webserver process, so we can kill it
 
 tests_passed=true
-expected="Hello swarna"
+expected="Hello From Golang"
 output=$(curl -s localhost:8080)
 if [[ "$expected" != "$output" ]]; then
   echo "Test Failure"
